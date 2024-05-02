cd7548c1902b81748e525bbf9617fd9cd1d83f13
Ebru Nida <ekarada1@my.centennialcollege.ca>
2024-05-01 23:40:09 -0400

    Created entrypoint of the application
    Using express.js and mongoose. Set up route for students.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const studentRoutes = require('./routes/studentRoutes');
+
+const app = express();
+const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/students', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});   
+
+app.use('/students', studentRoutes);
+
+//
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
